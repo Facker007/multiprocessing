cd872ff6a861ba1525811d6d31139d4edce545e3
Facker007 <42063107+Facker007@users.noreply.github.com>
2019-04-20 11:11:29 +0800

    Create udp_host_scan.py

---
+++ udp_host_scan.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import sys,time
+from threading import Thread
+from scapy.all import *
+def host_scan(ip):
+    try:
+        pkt=IP(dst=ip)/UDP(dport=56789)
+        result=sr1(pkt,timeout=1,verbose=0)
+        if int(result[IP].proto)==1:
+            time.sleep(0.1)
+            print ip,"zai"
+        return
+    except:
+        return 
+def main():
+    if len(sys.argv) != 2:
+        print "sad"
+        sys.exit()
+    address=str(sys.argv[1]).strip()
+    prefix=address.split(".")[0]+"."+address.split(".")[1]+"."+address.split(".")[2]+"."
+    for i in range(1,255):
+        ip=prefix+str(i)
+        t=Thread(target=host_scan,args=(ip,))
+        t.start()
+if __name__=="__main__":
+    main()
